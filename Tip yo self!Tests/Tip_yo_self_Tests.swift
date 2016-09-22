0ab9873e67bb96d11fbcd8b589f61cefb04b8ad2
Venkataraman Valady Ganesan <vgkrish619@gmail.com>
2016-09-21 22:37:02 -0700

    Initial Commit

---
+++ Tip yo self!Tests/Tip_yo_self_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Tip_yo_self_Tests.swift
+//  Tip yo self!Tests
+//
+//  Created by Venkataraman Valady Ganesan on 9/21/16.
+//  Copyright © 2016 Venkataraman Valady Ganesan. All rights reserved.
+//
+
+import XCTest
+@testable import Tip_yo_self_
+
+class Tip_yo_self_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
