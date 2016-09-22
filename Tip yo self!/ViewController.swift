c867aec84c3c17395958da747dbbb2e84b11e91a
Venkataraman Valady Ganesan <vgkrish619@gmail.com>
2016-09-22 00:00:26 -0700

    initial commit

--- Tip yo self!/ViewController.swift
+++ Tip yo self!/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +26,37 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let tipPercentages = [0.18, 0.20, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String.init(format: "$%.2f", tip)
+        totalLabel.text = String.init(format: "$%.2f", total)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+    }
 }
 
